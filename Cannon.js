5ee5a55c69c874a260793c073ae11eeb2e61bfac
Carzen1 <86026009+Carzen1@users.noreply.github.com>
2021-07-26 18:04:40 -0400

    Add files via upload

---
+++ Cannon.js
@@ -0,0 +1,18 @@
+class Cannon{
+    constructor(x,y,w,h,angle){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.angle = angle;
+    }
+
+    display(){
+        fill("black");
+        translate(this.x,this.y)
+        rotate(this.angle)
+        rect(-10,-20, this.w, this.h);
+        arc(this.x - 30, this.y + 90, 140, 200, PI, TWO_PI)
+        noFill()
+    }
+}
